c6d623349c870f90f3e1d583a5dcd808d21c861b
mlerette <81699627+mlerette@users.noreply.github.com>
2021-09-21 09:32:37 -0400

    changes to root json/visitorServices

--- frontend/src/services/VisitorService.js
+++ frontend/src/services/VisitorService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const VISITOR_API_BASE_URL = "http://springpool.herokuapp.com";
+const VISITOR_API_BASE_URL = "http://springpool.herokuapp.com/api/v1/visitors";
 
 class VisitorService {
 
