56bcb6ef8f986ec157ab4dab44f8017aa863ada9
mlerette <81699627+mlerette@users.noreply.github.com>
2021-09-22 15:05:54 -0400

    changed address in visitor services AND changed hard set port in app.properties

--- frontend/src/services/VisitorService.js
+++ frontend/src/services/VisitorService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const VISITOR_API_BASE_URL = "http://springpool.herokuapp.com/api/v1/visitors";
+const VISITOR_API_BASE_URL = "jdbc:postgresql://ec2-52-6-77-239.compute-1.amazonaws.com:5432/dess7pm4up3igd/api/v1/visitors";
 
 class VisitorService {
 
