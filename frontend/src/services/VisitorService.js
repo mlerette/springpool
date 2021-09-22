c424d4033cd0c52b21dfdb0cab37d08f0c83c613
mlerette <81699627+mlerette@users.noreply.github.com>
2021-09-22 15:31:12 -0400

    changing frontend/visitorservices const back to http://

--- frontend/src/services/VisitorService.js
+++ frontend/src/services/VisitorService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const VISITOR_API_BASE_URL = "jdbc:postgresql://ec2-52-6-77-239.compute-1.amazonaws.com:5432/dess7pm4up3igd/api/v1/visitors";
+const VISITOR_API_BASE_URL = "https://springpool.herokuapp.com";
 
 class VisitorService {
 
