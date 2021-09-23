998c021fe3344ec64bb73ef9595f19a1e05d82c1
mlerette <81699627+mlerette@users.noreply.github.com>
2021-09-23 09:37:06 -0400

    changed addressing to api

--- frontend/src/services/VisitorService.js
+++ frontend/src/services/VisitorService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const VISITOR_API_BASE_URL = "https://springpool.herokuapp.com";
+const VISITOR_API_BASE_URL = "https://springpool.herokuapp.com/api/v1";
 
 class VisitorService {
 
